3a985f12b35bb757a4ed9c7781865ccc31c0ecaf
Aniket B <aniket.b@outlook.com>
2019-03-18 06:01:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 function dwarfRollCall(dwarves) {
+  var str = []
+  for(var i = 0; i < dwarves.length; i++) {
+    str.push((i+1) + '. ' + dwarves[i])
+  }
+  return str.join(' ') + ' '
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var arr = []
+  for(var i = 0; i < planeteerCalls.length; i++) {
+    arr.push(planeteerCalls[i].toUpperCase() + '!');
+  }
+  return arr
 }
 
 function longPlaneteerCalls(words) {
+  for(var i =0; i < words.length; i++) {
+    if(words[i].length > 4) {
+      return true;
+    }
+    else {
+      return false; 
+    }
+  }
 }
 
+//function findTheCheese (foods) {
+//  var cheese = ['cheddar', 'gouda', 'camembert']
+//  for(var i = 0; i < foods.length; i++) {
+//    if(foods[i] === cheese[i]) {
+//      return foods[i];
+//    }
+//  }
+//  return 'no cheese!'
+//}
+
 function findTheCheese (foods) {
+  for (var i = 0; i < foods.length; i++) {
+    if (foods[i] ===  'gouda' || foods[i] ===  'cheddar' || foods[i] === 'camembert') {
+      return foods[i];
+    }
+  }
+  return 'no cheese!'
 }
